ec1fc7f2eb4bfb8facab7cf1b35372deab3ff69f
jstremonti <pumpkinfishjt@gmail.com>
2020-10-27 14:08:47 -0400

    I added the heading on the directions screen and a prototype to the scoring system

--- app/src/main/java/com/example/treasurehunt/DirectionsActivity.java
+++ app/src/main/java/com/example/treasurehunt/DirectionsActivity.java
@@ -2,10 +2,22 @@ package com.example.treasurehunt;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
-public class DirectionsActivity extends AppCompatActivity {
+public class DirectionsActivity extends AppCompatActivity implements SensorEventListener {
+
+    // device sensor manager
+    private SensorManager mSensorManager;
+
+    // variable for heading text
+    TextView heading;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,5 +35,41 @@ public class DirectionsActivity extends AppCompatActivity {
 
         TextView textView4 = (TextView) findViewById(R.id.textView7);
         textView4.setText(directions);
+
+        // TextView that will tell the user what their heading is
+        heading = (TextView) findViewById(R.id.textView8);
+
+        // initialize your android device sensor capabilities
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION),
+                SensorManager.SENSOR_DELAY_GAME);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        // get the angle around the z-axis rotated
+        float degree = Math.round(event.values[0]);
+        heading.setText("Heading: " + Float.toString(degree) + " degrees");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // to stop the listener and save battery
+        mSensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+    }
+
+    public void scorePressed(View view) {
+        Intent intent = new Intent(this, ScoreActivity.class);
+        startActivity(intent);
     }
 }
